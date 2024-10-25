ab24f2b87d17ab2c6ca9a78577a57317fe07091f
hyperion <94633790+thesaucemantv@users.noreply.github.com>
2024-10-24 19:34:45 -0700

    Update ContentView.swift
    change for content view

--- ContentView.swift
+++ ContentView.swift
@@ -1,21 +1,27 @@
-//
-//  ContentView.swift
-//  SpaceCraft
-//
-//  Created by Isa Rashid on 10/24/24.
-//
-
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        WebViewWrapper()
+            .edgesIgnoringSafeArea(.all) // This makes the web view full screen
+    }
+}
+
+// Wrap WKWebView to use it in SwiftUI
+struct WebViewWrapper: UIViewRepresentable {
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: url)
         }
-        .padding()
+        
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // Not needed for now, but required for UIViewRepresentable
     }
 }
 
